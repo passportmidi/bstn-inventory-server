bc31eace5fc0c2c0889f0defbcb1b7caf4e9f731
Brinda Patel <brindap37@gmail.com>
2025-03-04 19:28:52 -0500

    added warehouse id  route all commented out. will attempt to test and join with larger code once Knex setup by jacob

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors({ origin: CORS_ORIGIN }));
 
 const PORT = process.env.PORT || 8081;
 
+// all routes
+app.use(express.json());
+// app.use("/api/warehouses", warehouseRoutes);
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });

4e038c0c9736269a17fe09ba019a677adf778331
Brinda Patel <brindap37@gmail.com>
2025-03-05 12:25:46 -0500

    addded export functions on controller

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,33 +1,37 @@
 import initKnex from "knex";
 import configuration from "../knexfile.js";
-import knex from '../db/knex.js'; 
+import knex from "../db/knex.js";
 const knex = initKnex(configuration);
 
 const getAllWarehouses = async (req, res) => {
-    try {
-      const warehouses = await knex('warehouses').select('*'); 
-      res.status(200).json(warehouses);
-    } catch (error) {
-      console.error('Error fetching warehouses:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  };
+  try {
+    const warehouses = await knex("warehouses").select("*");
+    res.status(200).json(warehouses);
+  } catch (error) {
+    console.error("Error fetching warehouses:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 const findOneWarehouse = async (req, res) => {
-    try {
-      const warehousesFound = await knex("warehouse").where({ id: req.params.id });
-  
-      if (warehousesFound.length === 0) {
-        return res.status(404).json({
-          message: `Warehouse with ID ${req.params.id} not found`,
-        });
-      }
-  
-      const warehouseData = warehousesFound[0];
-      res.status(200).json(warehouseData);
-    } catch (error) {
-      res.status(500).json({
-        message: `Unable to retrieve warehouse data for warehouse with ID ${req.params.id}`,
+  try {
+    const warehousesFound = await knex("warehouse").where({
+      id: req.params.id,
+    });
+
+    if (warehousesFound.length === 0) {
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} not found`,
       });
     }
-  };
+
+    const warehouseData = warehousesFound[0];
+    res.status(200).json(warehouseData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve warehouse data for warehouse with ID ${req.params.id}`,
+    });
+  }
+};
+
+export { getAllWarehouses, findOneWarehouse };

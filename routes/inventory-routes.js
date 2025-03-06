175db566b896a61f6ff33a30b3fb8bc6e00c2bf7
Brinda Patel <brindap37@gmail.com>
2025-03-06 13:13:45 -0500

    deleting an inventory API

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -3,7 +3,11 @@ import * as inventoryController from "../controllers/inventory-controller.js";
 
 const router = express.Router();
 
-router.route("/").get(inventoryController.getAllInventories).post(inventoryController.createInventoryItem);
+router
+  .route("/")
+  .get(inventoryController.getAllInventories)
+  .post(inventoryController.createInventoryItem)
+  .delete(inventoryController.deleteInventory);
 router.route("/:id").get(inventoryController.findOneInventory);
 
 export default router;

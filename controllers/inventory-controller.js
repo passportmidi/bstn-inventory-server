2061979f6822eaaec1943eabefc410576307c871
Ghazaleh Ahmadzadeh <ghazalehahmadzadeh@ymail.com>
2025-03-06 11:13:20 -0500

    Added the api to get all inventories

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -2,6 +2,28 @@ import initKnex from "knex";
 import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
+const getAllInventories = async (req, res) => {
+  try {
+    const inventories = await knex("inventories")
+      .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      );
+
+    res.status(200).json(inventories);
+  } catch (error) {
+    res.status(500).json({
+      message: "Unable to retrieve inventory data",
+    });
+  }
+};
+
 const findOneInventory = async (req, res) => {
   try {
     const inventoriesFound = await knex("inventories").where({
@@ -23,4 +45,4 @@ const findOneInventory = async (req, res) => {
   }
 };
 
-export { findOneInventory };
+export { findOneInventory, getAllInventories };

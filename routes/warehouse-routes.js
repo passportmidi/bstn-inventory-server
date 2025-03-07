91cff21b6ef18fb27058a2ab9c57d416f1be6f91
Jacob Fattahi <jacobfattahi@gmail.com>
2025-03-07 17:16:33 -0400

    add create a warehouse in backend

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,7 +3,10 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
-router.route("/").get(warehouseController.getAllWarehouses);
+router.route("/").get(warehouseController.getAllWarehouses)
+// make a new warehouse
+.post(warehouseController.createWarehouse);
+
 router
   .route("/:id")
   .get(warehouseController.findOneWarehouse)
